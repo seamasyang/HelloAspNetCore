f2a4bf05d46260acfac9beda8ade6fc972348937
yakeir <yakeir@gmail.com>
2017-11-29 22:16:09 +0800

    add files under folder

---
+++ Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HelloAspNetCore.Models;
+
+namespace HelloAspNetCore.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly HelloAspNetCore.Models.MovieContext _context;
+
+        public CreateModel(HelloAspNetCore.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HelloAspNetCore.Models;
+
+namespace HelloAspNetCore.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly HelloAspNetCore.Models.MovieContext _context;
+
+        public IndexModel(HelloAspNetCore.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        /*public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }*/
+
+        public async Task OnGetAsync(string searchString)
+        {
+            var movies = from m in _context.Movie
+                        select m;
+
+        
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            Movie = await movies.ToListAsync();
+        }
+
+    }
+}
